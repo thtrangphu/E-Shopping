bacf689720e2594e4aed0e23b3e05d563e91cca4
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2023-04-21 00:36:49 +0700

    use handlebars to covert from static to dynamic

--- index.js
+++ index.js
@@ -4,6 +4,52 @@ let app = express();
 // Đưa template web tĩnh vào
 app.use(express.static(__dirname + "/public"));
 
+// View Engine
+let expressHbs = require("express-handlebars");
+let hbs = expressHbs.create({
+  extname: "hbs",
+  defaultLayout: "layout",
+  layoutDir: __dirname + "/views/layouts/",
+  partialsDir: __dirname + "/views/partials/",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+
+// Define your root here
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// app.get("/blog", (req, res) => {
+//   res.render("blog", { banner: "Our Blog" });
+// });
+
+app.get("/:page", (req, res) => {
+  let banners = {
+    blog: "Our Blog",
+    category: "Shop Category",
+    cart: "Shopping Cart",
+    checkout: "Product Checkout",
+    confirmation: "Order Confirmations",
+    contact: "Contact Us",
+    login: "Login / Register",
+    register: "Register",
+    // nếu có dấu "-" thì sử dụng "" cho tên thuộc tính sẽ giải quyết được
+    "single-blog": "Blog Details",
+    "single-product": "Shop Single ",
+    "tracking-order": "Order Tracking",
+  };
+  let page = req.params.page;
+  res.render(page, { banner: banners[page] });
+});
+// app.get("/blog", (req, res) => {
+//   res.render("blog");
+// });
+// app.get("/blog", (req, res) => {
+//   res.render("blog");
+// });
+
 // Luôn luôn để cuối phần cấu hình port
 // Khởi tạo Sever và Port Server
 // Biến môi trường cấp Port hoặc biến 5000 của localhost
